383255a3112df8d30015fbf08db1cb4cd37afe9f
Kuchitama <kiyo.kunihira@gmail.com>
2013-12-31 23:31:50 +0900

    setting postgresql

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,25 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import scalikejdbc._
+import java.sql.DriverManager
 
 object Application extends Controller {
+  private lazy val conf = play.api.Play.current.configuration
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Class.forName("org.postgresql.Driver")
+    val url = conf.getString("db.default.url")
+    val user = conf.getString("db.default.user")
+    val password = conf.getString("db.default.password")
+    Logger.debug(s"url[${url}]")
+
+    using(DB(DriverManager.getConnection(url.get, user.get, password.get))) { db =>
+      val name: Option[String] = db readOnly { implicit session =>
+        SQL("show tables;").bind(1).map(rs => rs.string("name")).single.apply()
+      }
+    }
+    Ok(views.html.index("MyTime is ready."))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -7,7 +7,10 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "com.github.seratch" %% "scalikejdbc"             % "[1.6,)",
+  "com.github.seratch" %% "scalikejdbc-play-plugin" % "[1.6,)",
+  "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
 )     
 
 play.Project.playScalaSettings
