2427e63fe0353d95802e555474853232028123d4
Kuchitama <kiyo.kunihira@gmail.com>
2014-01-05 20:42:43 +0900

    edit travis settings

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,29 +1,18 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import scalikejdbc._
-import java.sql.DriverManager
+import mvc.AppAction
 import utils.Loggable
+import play.api.mvc._
+import play.api.Logger
 
 object Application extends Controller with Loggable {
-  private lazy val conf = play.api.Play.current.configuration
 
-  def index = Action {
+  def index = AppAction {
+    implicit request =>
+      implicit db =>
 
-    logger.debug(s"called Application#index")
+    logger.info("called Application#index")
 
-    Class.forName("org.postgresql.Driver")
-    val url = conf.getString("db.default.url")
-    val user = conf.getString("db.default.user")
-    val password = conf.getString("db.default.password")
-    Logger.debug(s"url[${url}]")
-
-    using(DB(DriverManager.getConnection(url.get, user.get, password.get))) { db =>
-      val name: Option[String] = db readOnly { implicit session =>
-        SQL("show tables;").bind(1).map(rs => rs.string("name")).single.apply()
-      }
-    }
     Ok(views.html.index("MyTime is ready."))
   }
 
